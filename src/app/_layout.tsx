24646f402d01294444c058f27512f31ccf199f5c
Moogstir <bwmoogstir@gmail.com>
2026-07-10 14:23:28 -0600

    downgrading expo for compatability issues

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,8 @@ import {
   Inter_700Bold,
   useFonts,
 } from '@expo-google-fonts/inter';
-import { DarkTheme, Stack, ThemeProvider } from 'expo-router';
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
